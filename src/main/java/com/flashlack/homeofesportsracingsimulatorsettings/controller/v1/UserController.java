40813d97e57de267640c8a2a8a722acf0e71cbd2
FLASHLACK1314 <zlp18021547586@163.com>
2024-10-03 23:44:56 +0800

    feat(JWT): 测试类完成，JWT生效

---
+++ src/main/java/com/flashlack/homeofesportsracingsimulatorsettings/controller/v1/UserController.java
@@ -0,0 +1,19 @@
+package com.flashlack.homeofesportsracingsimulatorsettings.controller.v1;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户控制器
+ *
+ * @author FLASHLACK
+ */
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("v1/user")
+public class UserController {
+
+}
