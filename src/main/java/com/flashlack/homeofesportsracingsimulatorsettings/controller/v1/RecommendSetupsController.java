d248a3574000b2bff88ccf40241082233a5713be
FLASHLACK1314 <zlp18021547586@163.com>
2024-11-07 18:03:58 +0800

    feat: 添加推荐设置控制器
    - 新增 RecommendSetupsController 类- 添加推荐设置相关的 API 接口
    - 实现跨域支持

---
+++ src/main/java/com/flashlack/homeofesportsracingsimulatorsettings/controller/v1/RecommendSetupsController.java
@@ -0,0 +1,20 @@
+package com.flashlack.homeofesportsracingsimulatorsettings.controller.v1;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 推荐设置控制器
+ * @author FLASHLACK
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/recommendSetups")
+@CrossOrigin(origins = "*")
+public class RecommendSetupsController {
+    
+}
